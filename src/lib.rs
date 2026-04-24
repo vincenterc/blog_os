5277f439b28020a494fb05e6eb1ffc6c83afb568
vincenter <vincenterr@yahoo.com.tw>
2026-04-24 09:31:09 +0800

    Paging Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use core::panic::PanicInfo;
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -71,10 +72,15 @@ pub fn hlt_loop() -> ! {
     }
 }
 
+#[cfg(test)]
+use bootloader::{BootInfo, entry_point};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
